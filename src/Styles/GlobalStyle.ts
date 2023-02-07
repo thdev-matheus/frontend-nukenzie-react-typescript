73c29f1010c39199e9d3d259b504ab4c0283a0e0
Theus <matheusth.dev@gmail.com>
2023-02-07 13:15:40 -0300

    feat: estilização global

---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GloblaStyle = createGlobalStyle`
+
+    * {
+    margin: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+    box-shadow: 0 0 0 0;
+    box-sizing: border-box;
+    }
+
+    body {
+      width: 100vw;
+      height: 100vh;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+
+      overflow: hidden auto;
+    }
+
+    h1, h2, h3, h4 {
+      margin-bottom: 0;
+    }
+    
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { GloblaStyle } from "./Styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <>
+    <GloblaStyle />
     <App />
   </>
 );
